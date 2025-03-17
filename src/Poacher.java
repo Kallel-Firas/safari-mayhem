9c13465bf15bec037ee19afd8fbe592168fbe004
Moucatash Faris Sami Victor <vi83i9@inf.elte.hu>
2025-03-17 16:13:58 +0000

    Edit Poacher.java

--- src/Poacher.java
+++ src/Poacher.java
@@ -1,12 +1,37 @@
+import java.util.List;
+import java.util.Random;
+
 public class Poacher {
     private boolean isVisible;
+    private boolean hasCapturedAnimal;
 
-    public void attemptHunt() {
+    public Poacher(boolean isVisible) {
+        this.isVisible = false;
     }
 
-    public void captureAnimal() {
+    public boolean attemptHunt() {
+        Random rand = new Random();
+        return rand.nextBoolean();
+
     }
 
-    public void evadeRangers() {
+    public boolean captureAnimal() {
+        Random rand = new Random();
+        hasCapturedAnimal =  rand.nextInt(100) < 60;
+        return hasCapturedAnimal;
+    }
+
+    public boolean evadeRangers() {
+        Random rand = new Random();
+        return rand.nextBoolean();
+
+    }
+
+    public void setVisibility(boolean visibility) {
+        this.isVisible = visibility;
+    }
+
+    public boolean isVisible() {
+        return isVisible;
     }
 }
