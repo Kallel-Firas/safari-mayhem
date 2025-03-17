697f0d4c07bb7c3aad00ec57fd01f31ecf1abecb
Moucatash Faris Sami Victor <vi83i9@inf.elte.hu>
2025-03-17 19:46:39 +0000

    Edit Ranger.java

--- src/Ranger.java
+++ src/Ranger.java
@@ -3,12 +3,23 @@ import java.util.List;
 public class Ranger {
     private int salary;
 
+    public Ranger(int salary) {
+        this.salary = salary;
+    }
     public void eliminatePredator() {
+        System.out.println("Ranger is eliminating a predator.");
+
     }
 
-    public void protectFromPoachers() {
+    public void protectFromPoachers(Poacher poacher) {
+        if (poacher.isVisible()) {
+            System.out.println("Ranger is eliminating a poacher.");
+        } else {
+            System.out.println("No poachers detected nearby.");
+        }
     }
 
     public void patrolSafari() {
+        System.out.println("Ranger is patrolling the safari.");
     }
 }
