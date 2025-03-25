2a330b06f85780577faa21e9c25c6b46d558b6ac
Firas Kallel <hhfx00@inf.elte.hu>
2025-03-25 11:05:01 +0100

    Added scrollable game map and minimap

--- src/View/GameScreen.java
+++ src/View/GameScreen.java
@@ -17,26 +17,36 @@ import javax.swing.Timer;
 
 public class GameScreen extends JFrame {
     GameMap gameMap;
+    MiniMap miniMap;
     Safari safari = new Safari(1, 1, "1/1/2021");
     public GameScreen() {
 
-        List<Landscape> terrain = new ArrayList<>();
-        for (int i = 0; i < 50*50; i++) {
-            terrain.add(new Dirt());
-        }
 
-        setTitle("Model.Safari Mayhem");
+
+        setTitle("Safari Mayhem");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setPreferredSize(new Dimension(50*16, 50*16));
         setLayout(new BorderLayout());
-        gameMap = new GameMap(terrain, new ArrayList<>());
-        add(gameMap, BorderLayout.CENTER);
+        gameMap = new GameMap();
+        miniMap = new MiniMap(gameMap.getTerrainImages(), gameMap);
+        gameMap.setMiniMap(miniMap);
+        // TODO: remove magic numbers
+        gameMap.setBounds(0, 0, 800, 800);
+        miniMap.setBounds(600, 0, 200, 200);
+
+        JLayeredPane layeredPane = new JLayeredPane();
+        layeredPane.setPreferredSize(new Dimension(800, 800));
+
+        layeredPane.add(gameMap, JLayeredPane.DEFAULT_LAYER);
+        layeredPane.add(miniMap, JLayeredPane.PALETTE_LAYER);
+
+        add(layeredPane, BorderLayout.CENTER);
         pack();
     }
 
     public void run() {
         safari.Update();
         gameMap.update(safari.getLandscapes(), safari.getEntities());
+        miniMap.update(safari.getLandscapes());
         gameMap.repaint();
     }
 }
